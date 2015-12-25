56bb77527ac9204ad437427a9b6075cec1580db9
Aidi Stan <aidistan@live.com>
2015-12-26 02:31:03 +0800

    Update Home view

--- app/views/Home.vue
+++ app/views/Home.vue
@@ -13,33 +13,18 @@
         a(href="http://weibo.com/hiwuren", target="_blank")
           i.am-icon-weibo.am-icon-sm
         span.am-margin-right-lg
-        i.am-icon-weixin.am-icon-sm
-    .css5a577b1e423.am-text-center.am-margin-vertical-3xl
-      span 很久很久以前，奇怪的生物在地球上出现，
-      br
-      span 他们可以制造物件，可以改变世界，他们被称为人类！
-    img.am-img-responsive.am-margin-bottom-xl(src="../assets/home-slide-1.png")
+        i.am-icon-weixin.am-icon-sm(@click="showQRcode")
+    p.am-center.am-text-lg 很久很久以前，奇怪的生物在地球上出现，他们可以制造物件，可以改变世界，他们被称为人类！
+    img.am-img-responsive.am-center(src="../assets/home-slide-1.png", :style="imgStyle")
   #home-slide-2.slide
-    .css5a577b1e423.am-text-center.am-margin-vertical-3xl
-      span 人类在地球的日子里， 发明了很多物件，
-      br
-      span 它们丰富生活， 改变生活， 也随着生活更新换代！
-    img.am-img-responsive.am-margin-bottom-xl(src="../assets/home-slide-2.png")
+    p.am-center.am-text-lg 人类在地球的日子里，发明了很多物件，它们丰富生活，改变生活，也随著生活更新换代！
+    img.am-img-responsive.am-center(src="../assets/home-slide-2.png", :style="imgStyle")
   #home-slide-3.slide
-    .css5a577b1e423.am-text-center.am-margin-vertical-3xl
-      span 它们承载了往事，寄托了情怀，蕴藏了初心，
-      br
-      span 与人一同相伴，游历独一无二的时光！
-    img.am-img-responsive.am-margin-bottom-xl(src="../assets/home-slide-3.png")
+    p.am-center.am-text-lg 它们承载了往事，寄托了情怀，蕴藏了初心，与人一同相伴，游历独一无二的时光！
+    img.am-img-responsive.am-center(src="../assets/home-slide-3.png", :style="imgStyle")
   #home-slide-4.slide
-    .css5a577b1e423.am-text-center.am-margin-vertical-3xl
-      span 物境未觉，与您一起发现时光里的珍物，
-      br
-      span 共建游历初心的物件文化
-      br
-      span 感受万物欢喜，体会万物动情！
-    img.am-img-responsive.am-margin-bottom-xl(src="../assets/home-slide-4.png")
-    .home-links.am-text-center
+    p.am-center.am-text-lg 物境未觉，与您一起发现时光里的珍物，共建游历初心的物件文化，感受万物欢喜，体会万物动情！
+    .home-links.am-text-center.am-margin-vertical
       i.am-icon-apple.am-icon-sm.am-icon-grey
       span.am-margin-right-lg
       i.am-icon-android.am-icon-sm.am-icon-grey
@@ -49,19 +34,79 @@
       a(href="http://weibo.com/hiwuren", target="_blank")
         i.am-icon-weibo.am-icon-sm
       span.am-margin-right-lg
-      i.am-icon-weixin.am-icon-sm
-    #today-apply.am-text-center.am-margin-vertical-xl(v-link="{ name: 'apply' }")
-      span.am-padding.am-padding-horizontal-lg
-        img.am-margin-right-xs(src="../assets/logo-black-48.png", alt="申请", width="32", height="32")
-        span.am-padding-right-lg 申请建立我的博物馆
-    .am-text-sm.am-text-center.am-margin-top-xl Copyright &copy; 物境未觉
+      i.am-icon-weixin.am-icon-sm(@click="showQRcode")
+    img.am-img-responsive.am-center(src="../assets/home-slide-4.png", :style="imgStyle")
+    footer.am-text-xs.am-text-center.am-margin-xs &copy;2015 上海逗物信息技术有限公司，沪ICP备15037655号-1
+  #home-qrcode2wxmp.am-modal.am-modal-no-btn(tabindex="-1")
+    .am-modal-dialog
+      .am-modal-hd 物境未觉
+        a.am-close.am-close-spin(href="javascript: void(0)", data-am-modal-close=true) &times;
+      .am-modal-bd
+        img.am-img-responsive(src="../assets/qrcode.jpg", alt="物境未觉公众号二维码")
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      slide: { no: 1, max: 4 },
+      imgStyle: { maxHeight: 0 }
+    };
+  },
   ready: function() {
-    if ($('.slide').height() < $(window).height()) {
-      $('.slide').height($(window).height());
+    this.initSlides();
+    this.imgStyle =  {
+      maxHeight: $(window).height() -
+        $('#home-slide-1').find('img:last-child').position().top + 'px'
+    };
+  },
+  methods: {
+    initSlides: function() {
+      var self = this;
+
+      self.resizeSlides();
+      $(window).scrollTop(0).resize(self.resizeSlides);
+
+      var lastWheelTimeStamp;
+      $('body').on('wheel', function(e) {
+        if (!lastWheelTimeStamp || e.timeStamp - lastWheelTimeStamp > 500) {
+          lastWheelTimeStamp = e.timeStamp;
+          if (e.originalEvent.deltaY > 0) {
+            self.slideDown();
+          } else {
+            self.slideUp();
+          }
+        }
+        return false;
+      });
+
+      var Hammer = $.AMUI.Hammer;
+      var hammertime = new Hammer(document.getElementById('home'));
+      hammertime.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+      hammertime.on('swipeup', self.slideDown).on('swipedown', self.slideUp);
+    },
+    resizeSlides: function() {
+      $('#home .slide').css({
+        width: $(window).width(),
+        height: $(window).height()
+      });
+    },
+    slideUp: function() {
+      if (this.slide.no > 1) {
+        $(window).smoothScroll({
+          position: $('#home-slide-' + (--this.slide.no)).position().top
+        });
+      }
+    },
+    slideDown: function() {
+      if (this.slide.no < this.slide.max) {
+        $(window).smoothScroll({
+          position: $('#home-slide-' + (++this.slide.no)).position().top
+        });
+      }
+    },
+    showQRcode: function() {
+      $('#home-qrcode2wxmp').modal({ width: 268 });
     }
   },
   components: {
@@ -74,17 +119,17 @@ export default {
 <style lang="scss">
 @import '../variables.scss';
 
-.am-margin-vertical-3xl {
-  margin-top: 70px;
-  margin-bottom: 70px;
-}
-
 #home {
   width: 100%;
   background-image: url('../assets/background.jpg');;
 
+  .slide { position: relative; }
+  @media screen and (min-width: 641px) { .slide { padding: 40px; } }
+  @media screen and (max-width: 640px) { .slide { padding: 20px; } }
+
   .am-icon-apple,
   .am-icon-android {
+    cursor: default;
     color: $grey-light;
   }
 
@@ -95,9 +140,19 @@ export default {
     color: $grey-dark;
   }
 
-  .slide { padding: 40px; }
-  @media screen and (max-width: 640px) {
-    .slide { padding: 20px; }
+  p {
+    font-family:'JetLinkMediumSt81f81195423';
+    text-align: center;
+    max-width: 600px;
+    margin-top: 50px;
+    margin-bottom: 70px;
+  }
+
+  footer {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 }
 </style>
