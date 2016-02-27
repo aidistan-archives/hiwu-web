6618f069a92085dcc2de9abf41b0095c38609df3
Aidi Stan <aidistan@live.com>
2016-02-28 01:15:35 +0800

    Update Today view

--- app/views/Today.vue
+++ app/views/Today.vue
@@ -2,23 +2,22 @@
 #today
   #today-topbar
     topbar(title="每日博物展", :left-link="{ name: 'archive' }", :left-image="topbar.leftImage", left-alt="往期博物展")
-  #today-banner
-    .css195689d78423.am-text-lg.am-text-center.am-margin-vertical-xl
-      span 物境未覺，與您一起發現時光裏的珍物
+  #today-banner.am-margin-vertical-lg
+    .am-text-center
+      span 物境未觉，与您一起发现时光里的珍物
       br
-      span 共建遊歷初心的物件文化
+      span 共建游历初心的物件文化
       br
-      span 感受萬物歡喜，體會萬物動情
-    #today-apply.am-text-center.am-margin-vertical-xl(v-link="{ name: 'apply' }")
-      span.am-padding.am-padding-horizontal-lg
-        img.am-margin-right-xs(src="../assets/logo-black-48.png", alt="申请", width="32", height="32")
-        span.am-padding-right-lg 申请建立我的博物馆
-  #today-galleries.am-container
+      span 感受万物欢喜，体会万物动情
+    #today-apply.am-text-center.am-margin-vertical-lg(v-link="{ name: 'apply' }")
+      span.am-padding.am-padding-horizontal-lg 申请建立我的博物馆
+  hr#today-seperator
+  #today-galleries.am-container.am-padding-0
     .am-text-center.am-margin-xl(v-if="$loadingRouteData")
       i.am-icon-circle-o-notch.am-icon-spin.am-icon-lg
       h3.am-margin-top-xl 正在努力加载中...
-    .am-g.am-g-collapse.am-padding-sm(v-else)
-      gallery.am-margin-bottom-sm(v-for="entry in data", :data="entry.gallery", transition="")
+    template(v-else)
+      gallery.am-margin-bottom(v-for="entry in data", :data="entry.gallery", transition="")
       p.am-text-center 点击左上角查看更多往期博物馆
 </template>
 
